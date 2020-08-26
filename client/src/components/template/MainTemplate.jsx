954550901407f6a2b80f20aca53b137ecb66898c
tinkerbell93 <hhjj105@gmail.com>
2020-08-26 17:13:03 +0900

    feat: init mediaquery and event card

--- client/src/components/template/MainTemplate.jsx
+++ client/src/components/template/MainTemplate.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import Card from '../organism/Card';
 import './MainTemplate.scss';
+import SearchInput from '../molecule/main/SearchInput';
 
 export default function MainTemplate() {
   return (
-    <div className='cards'>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
+    <main className='main'>
+      <h2>이벤트 검색</h2>
+
+      <section className='cards'>
+        <h2>이벤트 리스트</h2>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </section>
+    </main>
   );
 }

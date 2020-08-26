318721135f8181fe27b2841a11a2cc5a75566ba8
tinkerbell93 <hhjj105@gmail.com>
2020-08-26 17:31:59 +0900

    feat: init main search

--- client/src/components/template/MainTemplate.jsx
+++ client/src/components/template/MainTemplate.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Card from '../organism/Card';
 import './MainTemplate.scss';
-import SearchInput from '../molecule/main/SearchInput';
+import Search from '../organism/Search';
 
 export default function MainTemplate() {
   return (
     <main className='main'>
       <h2>이벤트 검색</h2>
-
+      <Search />
       <section className='cards'>
         <h2>이벤트 리스트</h2>
         <Card />

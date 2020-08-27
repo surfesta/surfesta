35898d30de31865d30df519d5135d60abe21354e
tinkerbell93 <hhjj105@gmail.com>
2020-08-28 01:54:15 +0900

    feat: modified event detail html

--- client/src/components/molecule/eventDetail/OfflineInfo.jsx
+++ client/src/components/molecule/eventDetail/OfflineInfo.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function OfflineInfo() {
   return (
-    <li>
-      <span className='info-title'>위치</span>
-      <span className='info-text'>
-        마포구 월드컵북로 369 디지털파빌리온 3층
-      </span>
-    </li>
+    <tr>
+      <th>위치</th>
+      <td>
+        마포구 월드컵북로 369 디지털파빌리온 3층지털파빌리온 3층
+      </td>
+    </tr>
   );
 }
--- client/src/components/template/EventDetailTemplate.jsx
+++ client/src/components/template/EventDetailTemplate.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import './EventDetailTemplate.scss';
 import EventInfo from '../organism/eventDetail/EventInfo';
 import EventContents from '../organism/eventDetail/EventContents';
 
 export default function EventDetailTemplate() {
   return (
-    <div>
-      <EventInfo />
-      <EventContents />
-    </div>
+    <main className="main">
+      <div className="eventDetail">
+        <EventInfo />
+        <EventContents />
+      </div>
+    </main>
   );
 }

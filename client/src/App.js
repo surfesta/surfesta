4e124c324096aefb63467bc9323d1adf24c03dbf
Crescenteea <soarmei@gmail.com>
2020-08-29 14:51:06 +0900

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import './App.css';
-import Home from './pages/Home';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import MyProfile from "./pages/MyProfile";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/profile" component={MyProfile} />
         <Route path="/" component={Home} />
       </Switch>
     </BrowserRouter>

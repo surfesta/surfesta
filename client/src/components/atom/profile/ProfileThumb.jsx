63fc328941b63282c61b0f3ff7faa5f7e6ead052
Crescenteea <soarmei@gmail.com>
2020-09-01 13:42:08 +0900

    feat: Add style on profile page

--- client/src/components/atom/profile/ProfileThumb.jsx
+++ client/src/components/atom/profile/ProfileThumb.jsx
@@ -1,31 +1,33 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from 'react';
 
 function ProfileThumb() {
   const titleRef = useRef(null);
   useEffect(() => {
-    document.title = "내 정보 | Surfesta";
+    document.title = '내 정보 | Surfesta';
     const titleNode = titleRef.current;
-    titleNode.setAttribute("tabindex", -1);
+    titleNode.setAttribute('tabindex', -1);
     titleNode.setAttribute(
       // screenreader에서 읽힐 수 있는 제목 컨텐츠 추가
-      "aria-label",
-      "나의 프로필 정보를 수정하는 페이지입니다."
+      'aria-label',
+      '나의 프로필 정보를 수정하는 페이지입니다.'
     );
     titleNode.focus();
   }, []);
 
   const divStyle = {
-    border: "2px solid greenyellow",
-    width: "300px",
-    height: "300px",
-    margin: "0 auto",
+    border: '2px solid greenyellow',
+    width: '300px',
+    height: '300px',
+    margin: '0 auto',
   };
 
   return (
     <div>
-      <span id="profile-edit" ref={titleRef} className="title">
-        기본 정보
-      </span>
+      <div id="profile-edit">
+        <h2 ref={titleRef} className="title">
+          기본 정보
+        </h2>
+      </div>
       <div style={divStyle}>Thumbnail</div>
     </div>
   );

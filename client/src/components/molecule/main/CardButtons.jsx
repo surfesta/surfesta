2ab6a713162451163aaee0521bc56b822dca8f11
tinkerbell93 <hhjj105@gmail.com>
2020-09-04 16:57:57 +0900

    feat: implement favorite toggle button

--- client/src/components/molecule/main/CardButtons.jsx
+++ client/src/components/molecule/main/CardButtons.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
-import FavoriteButton from '../../atom/main/FavoriteButton';
-// import SettingButton from '../../atom/main/SettingButton';
-// import DeleteButton from '../../atom/main/DeleteButton';
+import React, { useState, useRef } from 'react';
+import { IconButton } from '@material-ui/core';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
-export default function CardButtons(props) {
+export default function CardButtons(event) {
+  // console.log(event);
+  const [select, setSelect] = useState(false);
+  const buttonRef = useRef();
+
   return (
     <div className="cardButtons-wrap">
-      <FavoriteButton />
-      {/* <SettingButton />
-      <DeleteButton /> */}
+      <div
+        className={select ? 'act favoriteButton-wrap' : 'favoriteButton-wrap'}
+      >
+        <IconButton aria-label="favorite" onClick={click} ref={buttonRef}>
+          <FavoriteIcon />
+        </IconButton>
+      </div>
+
+      {/* <IconButton aria-label="settings">
+        <MoreVertIcon />
+      </IconButton> */}
+
+      {/* <IconButton aria-label="delete">
+        <DeleteIcon />
+      </IconButton> */}
     </div>
   );
+
+  function click({ target }) {
+    setSelect(!select);
+    console.log(select);
+  }
 }

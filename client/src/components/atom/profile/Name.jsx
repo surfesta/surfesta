2a92bf486c68aa4c5aa67943b333e04ccda417af
Crescenteea <soarmei@gmail.com>
2020-09-08 18:29:55 +0900

    feat: Add profile form draft

--- client/src/components/atom/profile/Name.jsx
+++ client/src/components/atom/profile/Name.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React from 'react';
 
-export default function Name() {
+export default function Name({ user, handleChange }) {
   return (
     <div className="form-div">
       <label className="label-style">이름</label>
-      <input />
+      <input
+        type="text"
+        name="name"
+        value={user && user.username}
+        onChange={handleChange}
+      />
     </div>
   );
 }

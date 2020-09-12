1d3d94ccf41d33cc8ea8b161aa6581143ded4b5d
tinkerbell93 <hhjj105@gmail.com>
2020-09-12 21:13:56 +0900

    feat: on,offline filter

--- client/src/components/organism/Filter.jsx
+++ client/src/components/organism/Filter.jsx
@@ -2,10 +2,16 @@ import React, { useRef } from 'react';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import './Filter.scss';
 
-export default function Filter() {
+export default function Filter({ filter, setFilter, filterState }) {
+  const { ALL, ONLINE, OFFLINE } = filterState;
+
   const [selected, setSelected] = React.useState(false);
   const selectRef = useRef();
   const selectListRef = useRef();
+
+  const filterEvents = (type) => {
+    setFilter(type);
+  };
 
   return (
     <div className="filter-wrap">
@@ -16,9 +22,6 @@ export default function Filter() {
         }}
       >
         <span ref={selectRef}>Our Events </span>
-        <span role="img" aria-label="아이콘">
-          🎉
-        </span>
         <span className="more">
           <ExpandMoreIcon className={selected ? 'arrow rotataion' : 'arrow'} />
         </span>
@@ -29,9 +32,30 @@ export default function Filter() {
           ref={selectListRef}
           onClick={click}
         >
-          <li>Our Events</li>
-          <li>Online Events</li>
-          <li>Offline Events</li>
+          <li
+            onClick={() => {
+              filterEvents(ALL);
+            }}
+            className={filter === ALL ? 'act' : undefined}
+          >
+            Our Events
+          </li>
+          <li
+            onClick={() => {
+              filterEvents(ONLINE);
+            }}
+            className={filter === ONLINE ? 'act' : undefined}
+          >
+            Online Events
+          </li>
+          <li
+            onClick={() => {
+              filterEvents(OFFLINE);
+            }}
+            className={filter === OFFLINE ? 'act' : undefined}
+          >
+            Offline Events
+          </li>
         </ul>
       </div>
     </div>
--- client/src/components/template/EventsTemplate.jsx
+++ client/src/components/template/EventsTemplate.jsx
@@ -1,33 +1,59 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import Card from '../organism/main/Card';
 import './EventsTemplate.scss';
 import Search from '../organism/Search';
 import Filter from '../organism/Filter';
-import errorImg from '../../img/error.png';
 
 // Presentational Component
 export default function EventsTemplate({ events, loading, error }) {
+  const filterState = {
+    ALL: 'ALL',
+    ONLINE: 'ONLINE',
+    OFFLINE: 'OFFLINE',
+  };
+
+  const { ALL, ONLINE, OFFLINE } = filterState;
+
+  const [filter, setFilter] = useState(ALL);
+
   return (
     <main className="main">
       <h2 className="a11y-hidden">이벤트 검색</h2>
       <Search />
-      <Filter />
+      <Filter filter={filter} setFilter={setFilter} filterState={filterState} />
       <section className="cards-wrap">
         <h2 className="a11y-hidden">이벤트 리스트</h2>
 
         {error && (
           <div className="error-img">
-            <img src={errorImg} alt="에러" />
+            <img src="/img/error.png" alt="에러" />
             {/* <span>
-              Illustration by <a href="undefined">Maria Shukshina</a> from{' '}
+              Illustration by <a href="undefined">Maria Shukshina</a> from
               <a href="https://icons8.com/">Icons8</a>
             </span> */}
           </div>
         )}
         <div className="cards">
           {events &&
+            filter === ALL &&
             events.map((event) => {
               return event.isOpen && <Card event={event} key={event._id} />;
+            })}
+          {events &&
+            filter === ONLINE &&
+            events.map((event) => {
+              return (
+                event.isOpen &&
+                event.isOnline && <Card event={event} key={event._id} />
+              );
+            })}
+          {events &&
+            filter === OFFLINE &&
+            events.map((event) => {
+              return (
+                event.isOpen &&
+                !event.isOnline && <Card event={event} key={event._id} />
+              );
             })}
         </div>
       </section>

fda3f1c5a9d746a331f2f105966b93e1611d9e57
domuk-k <dannyworks102@gmail.com>
2020-09-12 18:43:11 +0900

    fix: code-splitting edits

--- client/src/utils/Portal.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom';
-
-export default function Portal({ children }) {
-  const modalRoot = document.querySelector('#modal-root');
-  return ReactDOM.createPortal(children, modalRoot);
-}

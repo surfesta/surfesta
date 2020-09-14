582519537abfcf77c9f3658e80a00dbec49ea63e
domuk-k <dannyworks102@gmail.com>
2020-09-15 01:53:06 +0900

    client-feature: 유저 로그인/로그아웃/회원가입/회원탈퇴 관련 인터렉션 개선

---
+++ client/src/components/atom/header/MobileBurger.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
+import { IconButton } from '@material-ui/core';
+
+export default function MobileBurger(props) {
+  return (
+    <IconButton className="only-mobile mobile-burger mobile-menu-button" {...props}>
+      <MenuIcon />
+    </IconButton>
+  );
+}
---
+++ client/src/components/organism/MobileHeaderRight.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function MobileHeaderRight(props) {
+  return <div {...props}>{props.children}</div>;
+}

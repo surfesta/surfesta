bf01fb97479225c6118dcc2c5353c767d3e2b6b8
tinkerbell93 <hhjj105@gmail.com>
2020-09-14 19:39:44 +0900

    feat: enlisted page draft

--- client/src/components/atom/main/DeleteButton.jsx
+++ client/src/components/atom/main/DeleteButton.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IconButton } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
+import Portal from '../../Portal';
 
 export default function DeleteButton() {
+  const [visible, setVisible] = useState(false);
+
   return (
-    <IconButton aria-label='delete'>
-      <DeleteIcon />
-    </IconButton>
+    <>
+      <IconButton
+        onClick={() => {
+          setVisible(true);
+        }}
+      >
+        <DeleteIcon />
+      </IconButton>
+
+      {visible && (
+        <Portal>
+          <div
+            id="modal-container"
+            onClick={(e) => {
+              if (!(e.target === e.currentTarget)) return;
+              setVisible(false);
+            }}
+          >
+            <div id="modal" className="confirm-modal">
+              <h1>이벤트 참가를 취소할까요?</h1>
+              <button className="cancel" onClick={() => setVisible(false)}>
+                취소
+              </button>
+              <button className="confirm">확인</button>
+            </div>
+          </div>
+        </Portal>
+      )}
+    </>
   );
 }
--- client/src/components/molecule/eventCategories/EnlistedEvents.jsx
+++ client/src/components/molecule/eventCategories/EnlistedEvents.jsx
@@ -4,10 +4,9 @@ import EnlistedContainer from '../../../containers/EnlistedContainer';
 export default function EnlistedEvents() {
   return (
     <>
-      <h1>참가신청한 이벤트</h1>
       <section className="cards-wrap">
         <div className="cards">
-          <EnlistedContainer />;
+          <EnlistedContainer />
         </div>
       </section>
     </>

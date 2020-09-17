2bcd330bd990c231fede388f74b3fa2f9783cab3
UB <yyb2535@gmail.com>
2020-09-17 18:13:11 +0900

    feat: qrcode-v0.4 test

--- client/src/pages/QrScanner.jsx
+++ client/src/pages/QrScanner.jsx
@@ -1,31 +1,42 @@
-import React, { Component } from "react";
+import React, { useCallback, useState } from "react";
 import QrReader from "react-qr-reader";
+import { useSelector } from "react-redux";
 import "./qrscanner.scss";
 
-export default class QrScanner extends Component {
-  state = {
-    result: "QR Code를 스캔해주세요.",
-  };
-
-  handleScan = (data) => {
-    console.log(data);
-    if (data) {
-      this.setState({
-        result: data,
-      });
-    }
-  };
-  handleError = (err) => {
+export default function QrScanner() {
+  const [result, setResult] = useState("QR Code를 스캔해주세요.");
+  const _event = useSelector((state) => state.events.events[1]);
+  console.log(_event);
+  const handleScan = useCallback(
+    (data) => {
+      if (data) {
+        const findData = _event.enlisted_users.find(
+          (item) => item._id === data
+        );
+        if (!findData) {
+          setResult("불일치");
+        } else {
+          setResult("일치");
+        }
+      }
+    },
+    [result]
+  );
+  const handleError = useCallback((err) => {
     console.error(err);
-  };
-  render() {
-    return (
-      <div className="qr-container">
-        <QrReader onError={this.handleError} onScan={this.handleScan} />
-        <div className="qr-result">
-          <p>{this.state.result}</p>
-        </div>
+  });
+
+  return (
+    <div className="qr-container">
+      <QrReader
+        delay={300}
+        onError={handleError}
+        onScan={handleScan}
+        resolution={1400}
+      />
+      <div className="qr-result">
+        <p>{result}</p>
       </div>
-    );
-  }
+    </div>
+  );
 }

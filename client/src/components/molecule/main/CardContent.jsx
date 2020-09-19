deddee8d1b2a11ed9f5bf548ab8272ad6e28637f
tinkerbell93 <hhjj105@gmail.com>
2020-09-19 15:59:31 +0900

    feat: refactoring main page

--- client/src/components/molecule/main/CardContent.jsx
+++ client/src/components/molecule/main/CardContent.jsx
@@ -1,48 +1,44 @@
 import React from 'react';
 
 export default function CardContent({ event }) {
-  const eventId = event._id;
   const thumbnail = event.thumbnail;
-  const { end, start } = event.event_date;
-  const startDate = start.date;
-  const startTime = start.time;
+  const { start } = event.event_date;
   const title = event.title;
   const price = event.price.toLocaleString();
   const isOnline = event.isOnline;
-  const hostProfileImg = event.host && event.host.profile_img;
-  const hostName = event.host && event.host.username;
-  const isOpen = event.isOnline;
+  const hostProfileImg = event.host.profile_img;
+  const hostName = event.host.username;
 
   return (
-    <div className="cardContent-wrap">
-      <div className="thumbnail-wrap">
+    <div className='cardContent-wrap'>
+      <div className='thumbnail-wrap'>
         <div
-          className="thumbnail"
+          className='thumbnail'
           style={{ backgroundImage: `url(${thumbnail})` }}
         ></div>
       </div>
-      <div className="content-wrap">
-        <p className="date">
+      <div className='content-wrap'>
+        <p className='date'>
           {start.date} {start.time}
         </p>
         <h3>{title}</h3>
-        <p className="price">
+        <p className='price'>
           <span>₩ </span>
           <span>{price}</span>
         </p>
-        <span className="online">{isOnline && '온라인'}</span>
+        <span className='online'>{isOnline && '온라인'}</span>
       </div>
-      <div className="host-wrap">
-        <p className="host">
+      <div className='host-wrap'>
+        <p className='host'>
           <span
-            className="host-profile"
+            className='host-profile'
             style={
               hostProfileImg
                 ? { backgroundImage: `url(${hostProfileImg})` }
                 : { backgroundColor: '#3562ff' }
             }
           ></span>
-          <span className="host-name">
+          <span className='host-name'>
             {hostName}
             {!hostName && '익명'}
           </span>
